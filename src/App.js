2193b73c20cddf2b1214be802d7f5ba18c705bda
youn9jo <youn9jo@gmail.com>
2021-06-29 13:39:33 +0900

    21.06.29 socketio with react first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useRef } from 'react';
+import './App.css';
+import ChatRoom from './ChatRoom';
+
+function App() {
+  const { messages, sendMessage } = ChatRoom();
+  const [newMessage, setNewMessage] = useState("");
+  const messageRef = useRef();
+
+  const handleSendMessage = () => {
+    if ( newMessage ) {
+      sendMessage(newMessage);
+      setNewMessage("");
+    }
+  };
+
+  const handleNewMessageChange = (e) => {
+    e.preventDefault();
+    setNewMessage(e.target.value)
+  }
+
+  const handleKeyUp = (e) => {
+    if(e.key === "Enter"){
+      sendMessage(newMessage);
+      setNewMessage("");
+    }
+  }
+
+  console.log(messages)
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="message-container">
+          <ul>
+            { messages.map((message, i) => (
+                  message.body !== "" ? (
+                    <li key={i}>
+                      { message.senderId } { message.body }
+                    </li>
+                  ) : null
+            ))}
+          </ul>
+          <div ref={ messageRef }></div>
+        </div>
+        <div className="action-container">
+          <input
+            className="message-input"
+            placeholder="enter message here"
+            variant="outlined"
+            value={ newMessage }
+            onChange={ handleNewMessageChange }
+            onKeyUp = { handleKeyUp }
+          />
+          <button
+            disabled = { newMessage === "" }
+            onClick={handleSendMessage}
+            className="send-button"
+          >
+            Send
+          </button>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
